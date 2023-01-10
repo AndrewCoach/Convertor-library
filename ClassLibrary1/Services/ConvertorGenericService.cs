b6b9b0276476f3491f3b51960c982fcd9126fd6e
Ondřej Kočí <ondrej.koci@creasoft.cz>
2023-01-10 20:23:10 +0100

    Adding basic services

---
+++ ClassLibrary1/Services/ConvertorGenericService.cs
@@ -0,0 +1,16 @@
+﻿using ConvertorLibs.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorLibs.Services
+{
+    public abstract class ConvertorGenericService : IConvertorGenericService
+    {
+        private List<IConversion> _conversions = new List<IConversion>();
+        public abstract bool CanConvert(string input1, string input2);
+        public abstract string Convert(string from, string to);
+    }
+}
---
+++ ClassLibrary1/Services/DistanceConvertorService.cs
@@ -0,0 +1,28 @@
+﻿using ConvertorLibs.Interfaces;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorLibs.Services
+{
+    public class DistanceConvertorService : ConvertorGenericService
+    {
+        public DistanceConvertorService()
+        {
+            _conversions.Add()
+        }
+
+        public override bool CanConvert(string input1, string input2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Convert(string from, string to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
