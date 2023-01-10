2c77067c89df7e2267c44b4dd6ba004ffb47e08e
Ondřej Kočí <ondrej.koci@creasoft.cz>
2023-01-10 20:09:20 +0100

    Adding rough interfaces

---
+++ ClassLibrary1/Interfaces/IConversion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorLibs.Interfaces
+{
+    internal interface IConversion
+    {
+        (string, string) GetTypes();
+
+        string Convert(string from, string to);
+    }
+}
---
+++ ClassLibrary1/Interfaces/IConvertorGenericService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorLibs.Interfaces
+{
+    internal interface IConvertorGenericService
+    {
+        bool CanConvert(string input1, string input2);
+
+        string Convert(string from, string to);
+    }
+}
